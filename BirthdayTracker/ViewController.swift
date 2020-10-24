4df7c478cff857af239976cf9b0232e351a4818f
Yegor Kozlovskiy <basterfly@gmail.com>
2020-10-24 18:17:12 +0300

    New project with ziped assets in root folder.

---
+++ BirthdayTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Yegor Kozlovskiy on 23.10.2020.
+//  Copyright © 2020 Yehor Kozlovskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
